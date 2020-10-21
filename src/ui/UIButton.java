81b585f8aaee33847539e29fec5aa035cd934c7f
Terry <Terry.C0W0@users.noreply.github.com>
2020-10-21 14:57:59 -0400

    cleaning up unused ui files

--- src/ui/UIButton.java
+++ src/ui/UIButton.java
@@ -1,21 +1,34 @@
 package ui;
 
+import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
 
-public class UIButton extends UIImage {
+public class UIButton {
 
+    private int x, y, width, height;
+    private Rectangle bounds;
     private ClickListener clicker;
+    private final BufferedImage image;
 
     public UIButton(int x, int y, int height, int width, BufferedImage image, ClickListener clicker) {
-        super(x, y, height, width, image);
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        bounds = new Rectangle(x, y, width, height);
+        this.image = image;
         this.clicker = clicker;
     }
 
-    public void onMouseClick(MouseEvent e){
+    void onMouseClick(MouseEvent e){
         if(bounds.contains(e.getX(), e.getY()))
             clicker.onClick();
 
+    }
+
+    void render(Graphics graphics) {
+        graphics.drawImage(image, x, y, width, height, null);
     }
 
 }
--- src/ui/UIManager.java
+++ src/ui/UIManager.java
@@ -6,28 +6,20 @@ import java.util.ArrayList;
 
 public class UIManager {
 
-    private ArrayList<UIImage> uiImages;
     private ArrayList<UIButton> uiButtons;
 
     public UIManager(){
-        uiImages = new ArrayList<>();
         uiButtons = new ArrayList<>();
     }
 
     public void render(Graphics graphics){
-        for(UIObject o: uiImages)
-            o.render(graphics);
-        for(UIObject o: uiButtons)
+        for(UIButton o: uiButtons)
             o.render(graphics);
     }
 
     public void onMouseClick(MouseEvent e){
         for(UIButton button: uiButtons)
             button.onMouseClick(e);
-    }
-
-    public void addUIImage(UIImage image){
-        uiImages.add(image);
     }
 
     public void addUIButton(UIButton button){
